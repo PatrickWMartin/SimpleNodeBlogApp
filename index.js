f64b237dd8c3a1dbd1bb5956d651d82d9088242d
Patrick Martin <patrickwaltermartin@gmail.com>
2024-01-12 08:58:14 -0400

    Rendering test data

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const port = 3000;
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.render("index");
+  //placeholder data
+  const blogData = [
+  ];
+
+  res.render("index", {blogData});
 });
 
 app.get("/create", (req, res) => {
