33423c609cbcbf53b9c1f125fe49df875c9cf3f4
Patrick Martin <patrickwaltermartin@gmail.com>
2024-01-14 20:49:25 -0400

    added the start of using mongoose

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require("express");
-
+const mongoose = require('mongoose');
+const Blog = require('./models/blogs');
 const app = express();
+
+
+
 const port = 3000;
+const dbConn = "";
+mongoose.connect(dbConn);
+
 app.set("view engine", "ejs");
 
+app.get('/test',(req, res) => {
+ const blog = new Blog({
+    title: 'Test blog',
+    body: 'Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.'
+  });
+
+  blog.save().then((result)=>{
+    res.send(result);
+  }).catch((err)=>{console.log(err)})
+});
 app.get("/", (req, res) => {
   //placeholder data
   const blogData = [
