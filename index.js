440d4024a2baf1ddb2cc230efee0159ac692242b
Patrick Martin <patrickwaltermartin@gmail.com>
2024-01-29 18:46:24 -0400

    Added delete functionality

--- index.js
+++ index.js
@@ -9,14 +9,31 @@ const app = express();
 
 
 const port = 3000;
-mongoose.connect(process.env.DB_CONNECTION_STRING );
+
+(async () => {
+  try {
+    console.log("Connecting to DB...");
+    await mongoose.connect(process.env.DB_CONNECTION_STRING );
+    
+    app.listen(port, () => {
+      console.log(`Listening on port ${port}...`);
+    });
+  } catch (error) {
+   console.log(error) 
+  }
+}
+)()
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", async (req, res) => {
-  let blogData = await Blog.find();
-  res.render("index", {blogData});
+  try {
+    let blogData = await Blog.find();
+    res.render("index", {blogData});
+  } catch (error) {
+    console.log(error);  
+  }
 });
 
 //placeholder for a single blog page
@@ -42,11 +59,18 @@ app.post("/create", async (req,res)=>{
   res.redirect('/');
 })
 
+app.delete("/blog/delete/:blogId",async (req, res)=>{
+  console.log(req.params.blogId);
+  try{
+    const test  = await Blog.findByIdAndDelete(req.params.blogId);
+    console.log(test);
+    res.json({redirect: '/'});
+  } catch(error){
+    console.log(error);
+  }
+});
 
 app.use((req, res) => {
   res.status(404).render("404");
 });
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}...`);
-});
